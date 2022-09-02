f2d19b9e2d130bd032db9e3b14882b936bb1352f
richieyuyongpoh <86595942+richieyuyongpoh@users.noreply.github.com>
2022-09-02 20:23:07 +0800

    Update objectrecognition.py

--- objectrecognition.py
+++ objectrecognition.py
@@ -62,3 +62,13 @@ webrtc_ctx = webrtc_streamer(
     media_stream_constraints={"video": True, "audio": False},
     async_processing=False,
 )
+
+
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
